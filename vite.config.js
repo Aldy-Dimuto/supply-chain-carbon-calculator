8be363480124b43a8359cddb4593dc5b95e3e07e
Aldy-Dimuto <aldy@dimuto.io>
2025-07-05 20:03:31 +0700

    Remove VueDevTools

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   base: '/supply-chain-carbon-calculator/',
   resolve: {
     alias: {
